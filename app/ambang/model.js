7e2b4f8a4faa9136e652f23d96453acfcac239d0
reihanalavi <reihanalavi@gmail.com>
2024-12-02 23:40:11 +0700

    first commit

---
+++ app/ambang/model.js
@@ -0,0 +1,88 @@
+const mongoose = require('mongoose');
+
+let ambangSchema = new mongoose.Schema({
+    dusunRTRW: {
+        type: String,
+        required: [
+            true,
+            "Dusun dan RT RW harus diisi"
+        ],
+        unique: true,
+    },
+    ambangMinimum: {
+        meteranUsaha: {
+            type: Number,
+            required: [
+                true,
+                "Ambang Minimum Meteran harus diisi"
+            ],
+            default: 0
+        },
+        meteranPribadi: {
+            type: Number,
+            required: [
+                true,
+                "Ambang Minimum Meteran harus diisi"
+            ],
+            default: 0
+        }
+    },
+    hargaPerKubikMinimum: {
+        meteranUsaha: {
+            type: Number,
+            required: [
+                true,
+                "Nominal Minimum Meteran harus diisi"
+            ],
+            default: 0
+        },
+        meteranPribadi: {
+            type: Number,
+            required: [
+                true,
+                "Nominal Minimum Meteran harus diisi"
+            ],
+            default: 0
+        }
+    },
+    hargaPerKubikMaksimum: {
+        meteranUsaha: {
+            type: Number,
+            required: [
+                true,
+                "Harga per kubik harus diisi"
+            ],
+            default: 0
+        },
+        meteranPribadi: {
+            type: Number,
+            required: [
+                true,
+                "Harga per kubik harus diisi"
+            ],
+            default: 0
+        }
+    },
+    biayaAdmin: {
+        meteranUsaha: {
+            type: Number,
+            required: [
+                true,
+                "Biaya admin harus diisi"
+            ],
+            default: 0
+        },
+        meteranPribadi: {
+            type: Number,
+            required: [
+                true,
+                "Biaya admin harus diisi"
+            ],
+            default: 0
+        }
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Ambang', ambangSchema);
